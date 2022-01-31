902bca44ac29cd907405f1804ba9235cb0e3e6b4
devlancer-work <devlancer.work@gmail.com>
2022-01-31 13:26:08 +0530

    SPA Updated

--- resources/js/end.js
+++ resources/js/end.js
@@ -1,24 +1,40 @@
 
-const mainScore = document.getElementById('score');
-const scoreOverlay = document.querySelector('.score-overlay');
-const restart = document.querySelector('.restart');
+function loadEndSnippet(){
+    let endfetch = new XMLHttpRequest();
+    endfetch.open('GET', './resources/snippets/end-snippet.html', true);
+    endfetch.onload = function(){
+        if(this.readyState = 200){
+            let endRes = this.responseText;
+            // console.log(endRes);
 
+            document.getElementById('main-data').innerHTML = endRes;
+            loadEndHtml();
 
-window.addEventListener('DOMContentLoaded', loadFinalScore);
+        }else {
+            console.log('Game Fetch - Error');
+        }
+    }
+    endfetch.send();
+};
 
-function loadFinalScore(){
-    let finalScore = localStorage.getItem('mainScore');        
-
-    // show score
-    mainScore.textContent = finalScore;
-    scoreOverlay.style.background = `conic-gradient(
-        var(--secondary-blue) ${finalScore}%,
-        var(--primary-blue) ${finalScore}%
-    )`;
+function loadEndHtml() {
+    const mainScore = document.getElementById('end__score');
+    const scoreOverlay = document.querySelector('.end__score-overlay');
+    const restart = document.querySelector(".restart");
+  
+  
+    function loadFinalScore() {
+      let finalScore = localStorage.getItem("mainScore");
+  
+      // show score
+      mainScore.textContent = finalScore;
+      scoreOverlay.style.background = `conic-gradient(
+          var(--secondary-blue) ${finalScore}%,
+          var(--primary-blue) ${finalScore}%
+      )`;
+    }
+    loadFinalScore(); 
+    restart.addEventListener('click', function(){
+        loadIndexHtml();
+    });
 }
-
-restart.addEventListener('click', function(){
-    window.location.assign('/index.html');
-})
-
-
--- resources/js/game.js
+++ resources/js/game.js
@@ -1,91 +1,84 @@
 
-const options = document.querySelectorAll('.option');
-const mainScore = document.getElementById('score');
-const progressBar = document.querySelector('.progress-bar');
-const scoreOverlay = document.querySelector('.score-overlay');
-const mainQuestionDemo = document.querySelector('.question_container');
+
+// Load game html
+
+function LoadGameHtml() {
+const options = document.querySelectorAll(".option");
+const mainScore = document.getElementById("score");
+const questionContainer = document.querySelector(".question_container");
+const progressBar = document.querySelector(".progress-bar");
+const scoreOverlay = document.querySelector(".score-overlay");
 const loader = document.getElementById('loader');
 
 
-// Utils
+  let score = 0;
+  let questionCounter = 0;
+  let currentQuestion = {};
+  let availableQuestions = [];
 
-let score = 0;
-let questionCounter = 0;
-let currentQuestion = {};
-let availableQuestions = [];
+  // Constants
+  const marksForCorrect = 10;
+  const totalQuestions = 10;
 
-
-// Constants
-const marksForCorrect = 10;
-const totalQuestions = 10;
-
-
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://opentdb.com/api.php?amount=10&category=17&difficulty=easy&type=multiple', true);
-xhr.onload = function(){
-
-    if(this.readyState == 4 && this.status == 200){
-        var res = JSON.parse(this.responseText);
-        availableQuestions = [...res.results];
-        // console.log(availableQuestions);
-        startGame();
-        
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://opentdb.com/api.php?amount=10&category=17&difficulty=easy&type=multiple",
+    true
+  );
+  xhr.onload = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var res = JSON.parse(this.responseText);
+      availableQuestions = [...res.results];
+      // console.log(availableQuestions);
+      startGame();
+    } else {
+      console.log("fetch api - error");
     }
-    else {
-        console.log("fetch api - error")
-    }
+  };
 
-}
+  xhr.send();
 
-xhr.send();
-
-
-function startGame(){
+  function startGame() {
     score = 0;
     questionCounter = 0;
     getNewQuestion();
-}
+  }
 
-function getNewQuestion(){
-
-    if(questionCounter != totalQuestions){
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
+  function getNewQuestion() {
+    if (questionCounter != totalQuestions) {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
     }
 
+    loader.classList.remove('hidden');
 
-
-    // loader.classList.remove('hidden')
-
-    if(questionCounter == totalQuestions){
-
-        // save data to local storage 
-        localStorage.setItem('mainScore', score);
-
-        return window.location.assign('./end.html');
+    if (questionCounter == totalQuestions) {
+      // save data to local storage
+        localStorage.setItem("mainScore", score);
+        loader.classList.add('hidden');
+        return loadEndSnippet();
     }
 
     questionCounter++;
 
     // ProgressBar
-    progressBar.style.width =  `${questionCounter*10}%`;
+    progressBar.style.width = `${questionCounter * 10}%`;
 
     let randomNum = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[randomNum];
     availableQuestions.splice(randomNum, 1);
 
     // Show Question
-    mainQuestionDemo.innerHTML = `<p id="question">${currentQuestion.question}</p>`;
+    questionContainer.innerHTML = `<p id="question">${currentQuestion.question}</p>`;
 
     loadChoices(currentQuestion);
-}
+  }
 
-
-function loadChoices(choices){
+  function loadChoices(choices) {
     var wrongChoices = [...choices.incorrect_answers];
     var correctChoice = choices.correct_answer;
     var currentChoices = wrongChoices;
@@ -96,59 +89,59 @@ function loadChoices(choices){
     // console.log(choices);
     // console.log(currentChoices);
 
-    options.forEach(function(option) {
-        let num = option.dataset.value;
-        option.innerHTML = `<p class="option-data">${currentChoices[num-1]}</p>`;
+    options.forEach(function (option) {
+      let num = option.dataset.value;
+      option.innerHTML = `<p class="option-data">${
+        currentChoices[num - 1]
+      }</p>`;
     });
-    
+
     loader.classList.add('hidden');
 
     pointerOn();
+  }
 
-}
+  options.forEach(function (option) {
+    option.addEventListener("click", function (e) {
+      let selectedVal = option.dataset.value;
 
-options.forEach(function(option){
-    option.addEventListener('click', function(e){
-        let selectedVal = option.dataset.value;
+      if (selectedVal - 1 == currentQuestion.answer) {
+        // console.log('Matched');
+        score += marksForCorrect;
+        // show score
+        mainScore.textContent = score;
+        scoreOverlay.style.background = `conic-gradient(
+                    var(--white) ${score}%,
+                    var(--primary-blue) ${score}%
+                )`;
 
-        if((selectedVal - 1) == currentQuestion.answer){
-            // console.log('Matched');
-            score += marksForCorrect;
-            // show score
-            mainScore.textContent = score;
-            scoreOverlay.style.background = `conic-gradient(
-                var(--white) ${score}%,
-                var(--primary-blue) ${score}%
-            )`;
-
-            option.classList.add('correct');
-                pointerNone();
-            setTimeout(() => {
-                option.classList.remove('correct');
-                getNewQuestion();
-            }, 1000);
-        }else {
-            // console.log('not Matched');
-            option.classList.add('incorrect');
-            pointerNone();
-            setTimeout(() => {
-                option.classList.remove('incorrect');
-                getNewQuestion();
-            }, 1000);
-        }
-
-
+        option.classList.add("correct");
+        pointerNone();
+        setTimeout(() => {
+          option.classList.remove("correct");
+          getNewQuestion();
+        }, 1000);
+      } else {
+        // console.log('not Matched');
+        option.classList.add("incorrect");
+        pointerNone();
+        setTimeout(() => {
+          option.classList.remove("incorrect");
+          getNewQuestion();
+        }, 1000);
+      }
     });
-});
+  });
 
-function pointerNone(){
-    options.forEach(function(opt){
-        opt.classList.add('pointer-none');
+  function pointerNone() {
+    options.forEach(function (opt) {
+      opt.classList.add("pointer-none");
     });
-}
+  }
 
-function pointerOn(){
-    options.forEach(function(opt){
-        opt.classList.remove('pointer-none');
+  function pointerOn() {
+    options.forEach(function (opt) {
+      opt.classList.remove("pointer-none");
     });
+  }
 }
